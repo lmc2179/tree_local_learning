6e7932a9d6a83ba4636dfe55e147512ca88608b6
lmc2179 <louiscialdella@gmail.com>
2019-03-03 23:49:12 -0600

    fixed sklearn cross val compatibility

--- tree_local_learning/demos/sine_demo.py
+++ tree_local_learning/demos/sine_demo.py
@@ -6,6 +6,7 @@ from sklearn.pipeline import Pipeline
 from matplotlib import pyplot as plt 
 import seaborn as sns
 import numpy as np
+from sklearn.model_selection import cross_val_predict
 
 X = np.array(sorted(np.random.normal(0,1, 100)))
 X_m = X.reshape(-1, 1)
@@ -15,5 +16,9 @@ p = Pipeline([('p', PolynomialFeatures()),
 m = TreeLocalLearnerRegressor(p, 0.25)
 m.fit(X_m, y)
 plt.scatter(X, y)
-plt.plot(X, m.predict(X_m), color='orange')
+#y_pred = m.predict(X_m)
+y_pred = cross_val_predict(m, X_m, y, cv=50)
+plt.plot(X, y_pred, color='orange', label='Tree of local regression')
+y_pred = cross_val_predict(p, X_m, y, cv=50)
+plt.plot(X, y_pred, color='green', label='Quadratic Regression')
 plt.show()
--- tree_local_learning/tree.py
+++ tree_local_learning/tree.py
@@ -2,19 +2,19 @@ from copy import deepcopy
 from sklearn.tree import DecisionTreeRegressor
 from collections import defaultdict
 import numpy as np
+from sklearn.base import BaseEstimator
+from sklearn.linear_model import LinearRegression
 
-class TreeLocalLearnerRegressor(object):
-    def __init__(self, model, partition_frac):
-        self.base_model = model 
+class TreeLocalLearnerRegressor(BaseEstimator):
+    def __init__(self, base_model=None, partition_frac=0.1):
+        self.base_model = base_model
         self.partition_frac = partition_frac
-        self.tree = None
-        self.partitions  = []
-        self.partition_models = {}
-    
+
     def fit(self, X, y):
         self.tree = DecisionTreeRegressor(min_samples_leaf=int(len(X)*self.partition_frac))
         self.tree.fit(X, y)
         self.partitions = list({idx for idx in self.tree.apply(X)})
+        self.partition_models = {}
         row_partition_lookup = defaultdict(list)
         for i, p in enumerate(self.tree.apply(X)):
             row_partition_lookup[p].append(i)
@@ -22,6 +22,7 @@ class TreeLocalLearnerRegressor(object):
             m = deepcopy(self.base_model)
             m.fit(X[row_partition_lookup[p]], y[row_partition_lookup[p]])
             self.partition_models[p] = m
+        return self
     
     def predict(self, X):
         predicted_partition = self.tree.apply(X)
