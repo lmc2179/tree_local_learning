6871a4488d0896e114c5267d14aab978f6640cf1
lmc2179 <louiscialdella@gmail.com>
2019-03-03 23:56:42 -0600

    switched to pure linear regression for demo

--- tree_local_learning/demos/sine_demo.py
+++ tree_local_learning/demos/sine_demo.py
@@ -8,17 +8,19 @@ import seaborn as sns
 import numpy as np
 from sklearn.model_selection import cross_val_predict
 
-X = np.array(sorted(np.random.normal(0,1, 100)))
+X = np.array(sorted(np.random.normal(0,2, 250)))
 X_m = X.reshape(-1, 1)
 y = np.sin(X)
 p = Pipeline([('p', PolynomialFeatures()),
               ('m', LinearRegression())])
-m = TreeLocalLearnerRegressor(p, 0.25)
+m = TreeLocalLearnerRegressor(LinearRegression(), 0.01)
 m.fit(X_m, y)
 plt.scatter(X, y)
 #y_pred = m.predict(X_m)
-y_pred = cross_val_predict(m, X_m, y, cv=50)
-plt.plot(X, y_pred, color='orange', label='Tree of local regression')
-y_pred = cross_val_predict(p, X_m, y, cv=50)
-plt.plot(X, y_pred, color='green', label='Quadratic Regression')
+y_pred = cross_val_predict(m, X_m, y, cv=250)
+plt.plot(X, y_pred, color='orange', label='Tree of local linear regression')
+y_pred = cross_val_predict(LinearRegression(), X_m, y, cv=250)
+plt.plot(X, y_pred, color='green', label='Linear Regression')
+plt.legend()
+plt.title('Leave one out cross validation')
 plt.show()
